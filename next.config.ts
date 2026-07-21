7bccf8983c9e1e190942bb30405a7c9c5e04a47a
tamlt2704 <tamlt2704@gmail.com>
2026-07-22 06:34:34 +0800

    use gh-pages and create deploy tasks

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  // basePath: 'https://javizstudio.github.io',
+  // assetPrefix: 'https://javizstudio.github.io'
 };
 
 export default nextConfig;
