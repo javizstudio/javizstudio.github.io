dcc6e988626ac78eefc73cde56892ba89db83e54
tamlt <tamlt2704@gmail.com>
2026-08-09 15:44:59 +0800

    Updates

--- _next/static/-YTNEsRueKuHZkya7UXws/_buildManifest.js
+++ _next/static/-YTNEsRueKuHZkya7UXws/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/javizstudio.github.io//_next/:path+",
+        "destination": "/javizstudio.github.io/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
