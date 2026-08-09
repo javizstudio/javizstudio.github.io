c4a27c0f92b30771197e29996e6a87f735131ba6
tamlt <tamlt2704@gmail.com>
2026-08-09 15:55:04 +0800

    update title

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,10 @@
 import type { NextConfig } from "next";
 
-const repo = "javizstudio.github.io"; // change if needed
-
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === "production" ? `/${repo}` : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? `/${repo}/` : "",
 };
 
 export default nextConfig;
