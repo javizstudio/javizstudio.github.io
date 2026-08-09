8b81721f5a2b5931cb06863b4bc64df5acc5ba69
tamlt <tamlt2704@gmail.com>
2026-08-09 15:44:28 +0800

    add gh deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const repo = "javizstudio.github.io"; // change if needed
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NODE_ENV === "production" ? `/${repo}` : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? `/${repo}/` : "",
 };
 
 export default nextConfig;
